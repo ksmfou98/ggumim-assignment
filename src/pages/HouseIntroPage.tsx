5e9e3320149ac281c6966f6c9d7ec718eeb42890
이도현 <ksmfou98@naver.com>
2022-01-30 21:04:26 +0900

    feat: House Intro Page Layout

--- src/pages/HouseIntroPage.tsx
+++ src/pages/HouseIntroPage.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import styled from "styled-components";
 
 function HouseIntroPage() {
-  return <div>HouseIntroPage</div>;
+  return <Container>HouseIntroPage</Container>;
 }
+
+const Container = styled.div`
+  width: 800px;
+  margin: 0 auto;
+  padding: 110px 0 0 0;
+`;
 
 export default HouseIntroPage;
