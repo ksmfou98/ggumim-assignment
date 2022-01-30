e52ccc311e1a86174481e99b4afb57ef0e341a7c
이도현 <ksmfou98@naver.com>
2022-01-30 20:22:19 +0900

    feat: GlobalStyles 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import HouseIntroPage from "pages/HouseIntroPage";
+import GlobalStyles from "styles/globalStyles";
 
 function App() {
   return (
     <>
+      <GlobalStyles />
       <HouseIntroPage />
     </>
   );
