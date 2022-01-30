35c7107751aee8ec135a5185dda22c25c7fbf8a9
이도현 <ksmfou98@naver.com>
2022-01-30 21:47:59 +0900

    feat: Product Image Content 컴포넌트 생성

---
+++ src/components/ProductImageContent.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+// @Note data 정보가 들어오면 재활용 가능하게 구현
+function ProductImageContent() {
+  return <div>ProductImageContent</div>;
+}
+
+export default ProductImageContent;
