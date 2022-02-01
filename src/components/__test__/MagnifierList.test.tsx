d993062f6fc7d1ce72802df79a0b6956d3f8bcd2
이도현 <ksmfou98@naver.com>
2022-02-01 23:34:07 +0900

    test: 첫 랜더링 때 tool tip이 안나오는지 테스트

---
+++ src/components/__test__/MagnifierList.test.tsx
@@ -0,0 +1,58 @@
+import { render, screen } from "@testing-library/react";
+import MagnifierList from "components/MagnifierList";
+
+describe("MagnifierList", () => {
+  const initialProps = {
+    productList: [
+      {
+        productId: 219762,
+        productName: "애틱 타일 이지케어 발수러그",
+        outside: true,
+        pointX: 486,
+        pointY: 197.667,
+        priceOriginal: 76000,
+        priceDiscount: 76000,
+        discountRate: 0,
+        imageUrl:
+          "//cdn.ggumim.co.kr/cache/furniture/300/20220110174102naCtctXTxY.png",
+      },
+      {
+        productId: 83544,
+        productName: "플로윙 화이트 원형 테이블",
+        outside: false,
+        pointX: 319,
+        pointY: 170.667,
+        priceOriginal: 120000,
+        priceDiscount: 74000,
+        discountRate: 38,
+        imageUrl:
+          "//cdn.ggumim.co.kr/cache/furniture/300/20200916133257f6yBWCxVRR.jpg",
+      },
+      {
+        productId: 134225,
+        productName: "트리니 벽난로콘솔",
+        outside: true,
+        pointX: 288,
+        pointY: 89.6666,
+        priceOriginal: 99000,
+        priceDiscount: 99000,
+        discountRate: 0,
+        imageUrl:
+          "//cdn.ggumim.co.kr/cache/furniture/300/202104291640198PeKIa8W06.JPG",
+      },
+    ],
+    selectedImageId: null,
+    onSelectImage: () => {},
+    imageSize: {
+      width: 800,
+      height: 998,
+    },
+  };
+
+  test("첫 랜더링 때 tool tip이 안나오는지 테스트", () => {
+    render(<MagnifierList {...initialProps} />);
+
+    const toolTipEle = screen.queryAllByRole("img", { name: "tooltip" });
+    expect(toolTipEle).toEqual([]);
+  });
+});
